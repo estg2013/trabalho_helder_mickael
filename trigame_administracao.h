205e5e0408e8a3734bfceeef05ca2748ce627eeb
burnspirit <burnspirit@gmail.com>
2013-05-21 22:52:47 +0100

    adicionada estrutura para os utilizadores

--- trigame_administracao.h
+++ trigame_administracao.h
@@ -5,7 +5,24 @@
 
 #endif // TRIGAME_ADMINISTRACAO_H_INCLUDED
 
+
+//estrutura de dados para as perguntas e respostas
 typedef struct{
     int categoria;
     char pergunta[200],resposta[4][200];
 } pergunta;
+
+
+//estrura de dados para os utilizadores
+typedef struct{
+    char username[100];
+    char password[100];
+    int admin;
+    int respostas_certas;
+    int respostas_erradas;
+} utilizador;
+
+
+
+
+
