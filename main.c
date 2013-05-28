24f45269efe9d844e3bab77647e5ee5c58138d85
burnspirit <burnspirit@gmail.com>
2013-05-28 22:08:13 +0100

    interface inicial

--- main.c
+++ main.c
@@ -1,71 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-//TRIGAME
-#include <trigame_administracao.h>
-#include <trigame_jogo.h>
-#include <trigame_interface.h>
+
 //SDL
 #include <SDL/SDL.h>
 #include <SDL/SDL_ttf.h>
 #include <SDL/SDL_Mixer.h>
 
+//TRIGAME
+#include <trigame_administracao.h>
+#include <trigame_jogo.h>
+#include <trigame_interface.h>
+
 #define RAND_MAX = 255;
-
-
-
 
 int main(int argc, char* args[])
 {
-    //SDL
-    SDL_Surface* fundo = NULL;
-    SDL_Surface* screen = NULL;
-    SDL_Surface* mensagem = NULL;
-    SDL_Rect rect = {50,50,100,50};
+    /*
+    *
+    * SDL
+    *   iniciar o frontend
+    */
 
-    SDL_Event event;
-
-    Mix_Music *music = NULL;
-
-    TTF_Font *font = NULL;
-
-    SDL_Color textColor = {255,255,255};
-
-    char texto[100];
-
-
-    screen = SDL_SetVideoMode(1024,768,32,SDL_SWSURFACE);
-
-    Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT,2,4096);
-
-
+    //definir estrutura para o ecra e inicializar SDL
+    SDL_Surface* ecra = NULL;
     SDL_Init(SDL_INIT_EVERYTHING);
 
-    TTF_Init();
+    SDL_VideoInfo* info = SDL_GetVideoInfo();
 
-    fundo = SDL_LoadBMP("img/splash.bmp");
+    //definir a resolucao do ecra e o numero de cores
+    ecra = SDL_SetVideoMode(1024,768,32,SDL_HWSURFACE);
 
-    music = Mix_LoadMUS("music/title.mp3");
+    //titulo da janela
+    SDL_WM_SetCaption("TriGame v0.1a",NULL);
 
-    font = TTF_OpenFont("font/font1.ttf",28);
-
-    Mix_PlayMusic(music,-1);
-
-    mensagem = TTF_RenderText_Solid(font,"Versao Alpha",textColor);
-
-    SDL_BlitSurface(fundo,NULL,screen,NULL);
-
-    SDL_BlitSurface(mensagem,NULL,screen,&rect);
-
-    SDL_WM_SetCaption("TriGame 2013",NULL);
-
-
-
-
-
-
-
-    /**** **********************************************/
-
+    /*
+    *  inicializar dados
+    */
     //filas
     filaUtilizadores fU;
     filaPerguntas fP;
@@ -74,46 +44,17 @@ int main(int argc, char* args[])
     if(fU = lerUtilizadores("users.db")) printf("Utilizadores - OK\n");
     if(fP = lerPergunta("perguntas.db")) printf("Perguntas - OK\n");
 
-    /********************   *******************************/
 
+    /*
+    * loop menu
+    */
+    menu(ecra);
 
-    while(1)
-    {
+    /*
+    *   fechar os processos do SDL
+    */
 
-        textColor =(SDL_Color) {rand(),rand(),rand()};
-        SDL_PollEvent(&event);
-        //eventos do teclado
-        if(event.type == SDL_KEYDOWN)
-        {
-            switch(event.key.keysym.sym)
-            {
-                case SDLK_ESCAPE:
-                    exit(1);
-                break;
-            }
-        }
+    SDL_Quit();
 
-        //eventos do rato
-        if(event.type == SDL_MOUSEMOTION)
-        {
-            rect =(SDL_Rect) {event.motion.x,event.motion.y,100,100};
-            sprintf(texto,".");
-        }
-
-        //clique do rato
-       if(event.type == SDL_MOUSEBUTTONUP)
-        {
-            rect =(SDL_Rect) {event.motion.x,event.motion.y,100,100};
-            sprintf(texto,event.button.type);
-        }
-
-        mensagem = TTF_RenderText_Solid(font,texto,textColor);
-        SDL_BlitSurface(mensagem,NULL,screen,&rect);
-        SDL_Flip(screen);
-    }
-
-    SDL_FreeSurface(fundo);
-    Mix_CloseAudio();
-    SDL_QUIT;
     return 0;
 }
--- trigame_interface.h
+++ trigame_interface.h
@@ -5,3 +5,49 @@
 
 #endif // TRIGAME_INTERFACE_H_INCLUDED
 
+/*
+* funcao para desenhar uma imagem no ecra nas coordenadas escolhidas
+*/
+void imagem(SDL_Surface* ecra, char imagem[], int x, int y)
+{
+    SDL_Rect rect = {x,y,100,100};
+    SDL_Surface* splash = NULL;
+    splash = SDL_LoadBMP(imagem);
+    SDL_BlitSurface(splash,NULL,ecra,&rect);
+    SDL_Flip(ecra);
+}
+
+
+/*
+* menu inicial
+*/
+void menu(SDL_Surface* ecra)
+{
+    char texto[100];
+    SDL_Event evento;
+    imagem(ecra,"img/splash.bmp",0,0); //fundo
+    imagem(ecra,"img/novojogo.bmp",350,300); //botao 1
+    imagem(ecra,"img/opcoes.bmp",350,370); //botao 1
+    imagem(ecra,"img/sair.bmp",350,440); //botao 1
+
+    while(1)
+    {
+        SDL_PollEvent(&evento);
+
+        if(evento.type == SDL_MOUSEMOTION)
+        {
+            sprintf(texto,"X:%i Y:%i",evento.motion.x,evento.motion.y);
+            SDL_WM_SetCaption(texto,NULL);
+        }
+
+        if(evento.type == SDL_KEYDOWN)
+        {
+            if(evento.key.keysym.sym == SDLK_ESCAPE)
+                exit(1);
+        }
+
+
+        SDL_Delay(10);
+        SDL_Flip(ecra);
+    }
+}
