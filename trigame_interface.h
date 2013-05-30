af6e71757ff3f367bf28287a6c196f0d63b9153f
face <mck.dcs@gmail.com>
2013-05-30 21:18:35 +0100

    colocação de commentarios no trigame_interface.h

--- trigame_interface.h
+++ trigame_interface.h
@@ -15,7 +15,9 @@ void menu(SDL_Surface* ecra)
     int ratoX, ratoY,but1 = 0, but2 = 0, but3 = 0;
 
     SDL_Event evento;
-    SDL_Rect rect = {0,0,100,100};
+    SDL_Rect rect = {0,0,100,100}; //posicionamento do ecra
+
+    //carregamento das imagens
 
     SDL_Surface* fundo = SDL_LoadBMP("img/splash.bmp");
     SDL_Surface* btNovoJogo = SDL_LoadBMP("img/novojogo.bmp");
@@ -26,69 +28,69 @@ void menu(SDL_Surface* ecra)
     SDL_Surface* btSairb = SDL_LoadBMP("img/sairb.bmp");
     SDL_Surface* msg = NULL;
 
-    //som
+    //carregamento do som
     Mix_Music* musica_menu = NULL;
     musica_menu = Mix_LoadMUS("music/botoes.wav");
 
-
-    TTF_Font *font = NULL;
-    SDL_Color cor1 = {0,0,0};
-    font = TTF_OpenFont("font/font1.ttf",26);
-    msg = TTF_RenderText_Solid(font,"Versao Alpha",cor1);
+    // colocação das fontes tipo, cor e messagem
+    TTF_Font *font = NULL;// inicializar a font
+    SDL_Color cor1 = {0,0,0};//cor
+    font = TTF_OpenFont("font/font1.ttf",26);//tipo de fonte
+    msg = TTF_RenderText_Solid(font,"Versao Alpha",cor1);//texto
 
     while(1)
     {
         SDL_PollEvent(&evento);
-        SDL_BlitSurface(fundo,NULL,ecra,NULL);
+        SDL_BlitSurface(fundo,NULL,ecra,NULL);//carregamento do fundo
         //botao novo jogo
-        rect.x = 350;
+        rect.x = 350; //posicao da imagem
         rect.y = 300;
-        SDL_BlitSurface(btNovoJogo,NULL,ecra,&rect);
+        SDL_BlitSurface(btNovoJogo,NULL,ecra,&rect);//carregamento do botao novo jogo
         //botao opcoes
-        rect.x = 350;
+        rect.x = 350;//posicao da imagem
         rect.y = 370;
-        SDL_BlitSurface(btOpcoes,NULL,ecra,&rect);
+        SDL_BlitSurface(btOpcoes,NULL,ecra,&rect);//carregamento do botao opcoes
         //botao sair
-        rect.x = 350;
+        rect.x = 350;//posicao da imagem
         rect.y = 440;
-        SDL_BlitSurface(btSair,NULL,ecra,&rect);
+        SDL_BlitSurface(btSair,NULL,ecra,&rect); //carregamento do botao sair
 
         if(evento.type == SDL_MOUSEMOTION)
         {
-            ratoX = evento.motion.x;
-            ratoY = evento.motion.y;
-            sprintf(texto,"X:%i Y:%i",ratoX,ratoY);
+            ratoX = evento.motion.x;// posição x do ponteiro do rato
+            ratoY = evento.motion.y;// posição y do ponteiro do rato
+            sprintf(texto,"X:%i Y:%i",ratoX,ratoY);//mostrar valor da posicao do rato
             SDL_WM_SetCaption(texto,NULL);
 
             //botao novo jogo
             if(ratoX > 351 && ratoX < 661 && ratoY > 302 && ratoY < 373)
             {
                 //botao novo jogo preto
-                    rect.x = 350;
+                    rect.x = 350;//posicao da imagem
                     rect.y = 300;
-                    SDL_BlitSurface(btNovoJogob,NULL,ecra,&rect);
+                    SDL_BlitSurface(btNovoJogob,NULL,ecra,&rect);//carregamento da imagem novo jogo preto
                     if(but1 == 0)
-                        Mix_PlayMusic(musica_menu,1);
+                        Mix_PlayMusic(musica_menu,1);//se o ponteiro do rato estiver em cima da imagem playmusic
                     but1 = 1;
                     but2 = but3 = 0;
             }else if(ratoX > 351 && ratoX < 661 && ratoY > 374 && ratoY < 444)
             {
                 //botao opcoes preto
-                    rect.x = 350;
+                    rect.x = 350;//posicao da imagem
                     rect.y = 370;
-                    SDL_BlitSurface(btOpcoesb,NULL,ecra,&rect);
+                    SDL_BlitSurface(btOpcoesb,NULL,ecra,&rect);//carregamento da imagem opcoes preto
                     if(but2 == 0)
-                        Mix_PlayMusic(musica_menu,1);
+                        Mix_PlayMusic(musica_menu,1); //se o ponteiro do rato estiver em cima da imagem playmusic
                     but2 = 1;
                     but1 = but3 = 0;
             }else if(ratoX > 351 && ratoX < 661 && ratoY > 444 && ratoY < 515)
             {
                 //botao sair preto
-                    rect.x = 350;
+                    rect.x = 350;//posicao da imagem
                     rect.y = 440;
-                    SDL_BlitSurface(btSairb,NULL,ecra,&rect);
+                    SDL_BlitSurface(btSairb,NULL,ecra,&rect); //carregamento da imagem sair preto
                     if(but3 == 0)
-                        Mix_PlayMusic(musica_menu,1);
+                        Mix_PlayMusic(musica_menu,1); //se o ponteiro do rato estiver em cima da imagem playmusic
                     but3 = 1;
                     but2 = but1 = 0;
             }else{
@@ -100,8 +102,8 @@ void menu(SDL_Surface* ecra)
 
         if(evento.type == SDL_MOUSEBUTTONDOWN)
         {
-            ratoX = evento.motion.x;
-            ratoY = evento.motion.y;
+            ratoX = evento.motion.x;// posição x do ponteiro do rato
+            ratoY = evento.motion.y;// posição y do ponteiro do rato
 
             //clique para sair
             if(ratoX > 351 && ratoX < 661 && ratoY > 444 && ratoY < 515)
@@ -117,7 +119,7 @@ void menu(SDL_Surface* ecra)
         }
 
         //versao do programa
-        rect.x = 735;
+        rect.x = 735;//posicao da imagem
         rect.y = 713;
         SDL_BlitSurface(msg,NULL,ecra,&rect);
 
